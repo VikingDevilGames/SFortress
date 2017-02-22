20216cb02bc397a875ec03e3f0a6c756e98da169
John Kayne <johnhoganuke2@gmail.com>
2017-02-22 14:59:23 -0500

    Update ImageManipulator.java

--- r5/src/main/java/sfortress/graphics/ImageManipulator.java
+++ r5/src/main/java/sfortress/graphics/ImageManipulator.java
@@ -1,3 +1,15 @@
 package sfortress.graphics;
 
+import java.awt.*;
 import java.awt.image.*;
+
+public class ImageManipulator {
+  public static BufferedImage makeGlowLayer(BufferedImage image) {
+    GaussianFilter filter = new GaussianFilter();
+    BufferedImage image2 = new BufferedImage(image.getWidth(),image.getHeight(),BufferedImage.TYPE_INT_ARGB);
+    Graphics g = image2.createGraphics();
+    filter.apply(g);
+    g.dispose();
+    return image2;
+  }
+}
