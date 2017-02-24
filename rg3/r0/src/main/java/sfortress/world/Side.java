117d6315b3a36e300021cefc5bd7c9c404fd32e0
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 17:06:18 -0500

    Update Side.java

--- rg3/r0/src/main/java/sfortress/world/Side.java
+++ rg3/r0/src/main/java/sfortress/world/Side.java
@@ -1,5 +1,17 @@
 package sfortress.world;
 
 public enum Side {
+  TOP("Top","Upper"),
+  NORTH("North","Northern"),
+  SOUTH("South","Southern"),
+  EAST("East","Eastern"),
+  WEST("West","Western"),
+  BOTTOM("Bottom","Lower");
   
+  public String name;
+  public String directionSide;
+  private Side(String name, String directionSide) {
+    this.name = name;
+    this.directionSide = directionSide;
+  }
 }
