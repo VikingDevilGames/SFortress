3fd2579f1be503bec9f8b594324012614729c62f
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 13:10:56 -0500

    Update SolidTile.java

--- rg3/r0/src/main/java/sfortress/game/tile/SolidTile.java
+++ rg3/r0/src/main/java/sfortress/game/tile/SolidTile.java
@@ -1,11 +1,17 @@
 package sfortress.game.tile;
 
+import sfortress.game.item.*;
 import sfortress.game.tile.*;
 
 public class SolidTile extends Tile {
   public float hardness;
+  public Item drops;
   public float meltingPoint;
   public FluidTile fluidForm;
+  
+  public void melt() {
+    slot.setTile(fluidForm);
+  }
   
   public void tick() {
     if (slot.temp >= meltingPoint) melt();
