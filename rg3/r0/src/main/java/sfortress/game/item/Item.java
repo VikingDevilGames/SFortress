20de875e177eaf3b530a7e3a7a4d304e88423e26
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 13:13:15 -0500

    Update Item.java

--- rg3/r0/src/main/java/sfortress/game/item/Item.java
+++ rg3/r0/src/main/java/sfortress/game/item/Item.java
@@ -3,5 +3,14 @@ package sfrotress.game.item;
 import sfortress.graphics.image.ItemIcon;
 
 public abstract class Item {
+  public String id;
+  public String name;
+  public ItemIcon icon;
   
+  public static void drawInWorld(int side) {
+    return icon.draw(true,side);
+  }
+  public static void draw() {
+    return icon.draw(false);
+  }
 }
