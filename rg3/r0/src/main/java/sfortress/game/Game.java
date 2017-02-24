ca71c72c22306eedf4e2da075a18c9f6d289d25f
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 13:07:38 -0500

    Update Game.java

--- rg3/r0/src/main/java/sfortress/game/Game.java
+++ rg3/r0/src/main/java/sfortress/game/Game.java
@@ -1,5 +1,14 @@
 package sfortress.game;
 
+import sfortress.world.Position;
+import sfortress.world.World;
+
 public class Game {
+  public static World world;
+  public static Position viewerPos;
   
+  public static void init() {
+    world = new World();
+    viewerPos = new Position(0,world.getGroundHeight(0,0),0);
+  }
 }
