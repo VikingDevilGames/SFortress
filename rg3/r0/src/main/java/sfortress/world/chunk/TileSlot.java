c3e9a7548de79f3cdc173aae6a86b383e3a5e2b1
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 13:21:29 -0500

    Update TileSlot.java

--- rg3/r0/src/main/java/sfortress/world/chunk/TileSlot.java
+++ rg3/r0/src/main/java/sfortress/world/chunk/TileSlot.java
@@ -4,5 +4,42 @@ import java.awt.image.BufferedImage;
 import sfortress.game.tile.*;
 
 public class TileSlot {
+  private Tile tile;
+  private boolean emtpy;
+  private float temp;
+  private final Position loc;
   
+  public TileSlot() {
+    setTile(null);
+  }
+  public TileSlot(Position loc) {
+    this(null,loc);
+  }
+  public TileSlot(Tile tile,Position loc) {
+    setTile(tile);
+    this.loc = loc;
+  }
+  
+  public void setTile(Tile tile) {
+    this.emtpy = (tile != null);
+    this.tile = (!this.empty)?tile:new VoidTile();
+  }
+  public void destroyTile() {
+    tile.destroy();
+    if (!empty) {
+      removeTile();
+    }
+  }
+  public Tile getTile() {
+    return Tile;
+  }
+  public void removeTile() {
+    setTile(null);
+  }
+  public float getTemp() {
+    return temp;
+  }
+  public void setPosition(Position p) {
+    this.loc = p;
+  }
 }
