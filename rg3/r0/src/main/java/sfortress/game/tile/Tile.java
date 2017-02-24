4920b2721ca2fb1e5bd6eb83baf0981647401808
John Kayne <johnhoganuke2@gmail.com>
2017-02-24 13:05:24 -0500

    Update Tile.java

--- rg3/r0/src/main/java/sfortress/game/tile/Tile.java
+++ rg3/r0/src/main/java/sfortress/game/tile/Tile.java
@@ -1,2 +1,21 @@
 package sfortress.game.tile;
 
+import sfortress.graphics.image.TileIcon;
+
+public abstract class Tile {
+  public String id;
+  public String name;
+  public TileIcon icon;
+  
+  private TileSlot slot;
+  
+  public abstract Tile fabricate();
+  public abstract void tick();
+  
+  public void set(TileSlot slot) {
+    this.slot = slot;
+  }
+  public BufferedImage[] draw(int side) {
+    return icon.draw(side);
+  }
+}
