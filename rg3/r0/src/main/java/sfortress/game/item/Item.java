c12d6b9acf166a1bf18afc81136c41f585c0ac3c
John Kayne <johnhoganuke2@gmail.com>
2017-02-25 17:15:14 -0500

    Update Item.java

--- rg3/r0/src/main/java/sfortress/game/item/Item.java
+++ rg3/r0/src/main/java/sfortress/game/item/Item.java
@@ -1,16 +1,24 @@
-package sfrotress.game.item;
+package sfortress.game.item;
 
 import sfortress.graphics.image.ItemIcon;
 
-public abstract class Item {
-  public String id;
-  public String name;
+public class Item {
   public ItemIcon icon;
-  
-  public static void drawInWorld(int side) {
-    return icon.draw(true,side);
+  public String name;
+  public String id;
+
+  public String[] categories;
+
+  public static boolean inCategories(Item item,String[] categories) {
+    boolean k = false;
+    for (int i = 0; i < item.categories.length; i++) {
+      for (int k = 0; k < categories.length; k++) {
+        k = (k && (item.categories[i] == categories[k]));
+      }
+    }
+    return k;
   }
-  public static void draw() {
-    return icon.draw(false);
+  public boolean inCategories(String[] categories) {
+    return inCategories(this,categories);
   }
 }
