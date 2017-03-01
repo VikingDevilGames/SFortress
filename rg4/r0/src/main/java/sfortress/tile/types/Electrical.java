1cc2aa9e93fd27050817411ca84a8171e1ec56ee
John Kayne <johnhoganuke2@gmail.com>
2017-03-01 12:21:22 -0500

    Update Electrical.java

--- rg4/r0/src/main/java/sfortress/tile/types/Electrical.java
+++ rg4/r0/src/main/java/sfortress/tile/types/Electrical.java
@@ -1,5 +1,20 @@
 package sfortress.tile.types;
 
 public interface Electrical {
+  private float resistivity;
+  private float voltage;
+  private float current;
   
+  public static float getVoltage(Elecrical e) {
+    return e.voltage - (e.voltage / e.resistance / e.resistance);
+  }
+  public static float getCurrent(Elecrical e) {
+    return getVoltage(e) / e.resistance;
+  }
+  public static float getPower(Electrical e) {
+    return getVoltage(e) * getCurrent(e);
+  }
+  public static float getEnergy(int seconds) {
+    return getPower(e) * seconds;
+  }
 }
